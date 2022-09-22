9eebd2ff4f88141aea84d82b4979911bb66730fd
Hanae <hanae.atay@gmail.com>
2022-09-22 23:22:22 +0200

    modifier code

--- mgi_Atay_Fakhri.py
+++ mgi_Atay_Fakhri.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-#################################################
+############################################
 # Calcul du MGD du robot RRR
 # INPUT: q = vecteur de configuration (radian, radian, radian)
 # OUTPUT: Xc = vecteur de situation = (x,y, theta)
